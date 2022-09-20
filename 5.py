b4174dad191eeb35bd260bfe0ef71500dc514623
●────────vamsi亗 <112240971+vamsi788@users.noreply.github.com>
2022-09-20 11:46:41 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,9 @@
+a=int(input())
+print("loaves discont")
+print("day old loaves discount")
+r=185*a
+d=0.6*185*a
+a=0.4*185*a
+print("regular price"+str(r))
+print("total discount"+str(d))
+print("total amount to be paid"+str(a))
