ce1cc5d16b8fda05eb0a120c8c02f3c5b45314d7
●────────vamsi亗 <112240971+vamsi788@users.noreply.github.com>
2022-09-21 10:48:45 +0530

    Add files via upload

---
+++ day 2 1.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+def countWays(s):
+    return fib(s + 1)
+ 
+# Driver program
+s = 2
+print("Number of ways = ")
+print (countWays(s))
