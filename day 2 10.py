c1ba575b1f69f4dfcb6953d1cde10339d2a7c169
●────────vamsi亗 <112240971+vamsi788@users.noreply.github.com>
2022-09-21 10:50:28 +0530

    Add files via upload

---
+++ day 2 10.py
@@ -0,0 +1,13 @@
+string1="python"
+string2="prog"
+result =[]
+if (len(string1)<len(string2)):
+    for i in string (2):
+        result.append(i)
+else:
+           
+                result.append (id)
+                print ("removecommon character")
+                print("different characters")
+
+    
