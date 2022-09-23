3dc37c069275886f7ebe6666c63d0a8f3c14bdd5
●────────vamsi亗 <112240971+vamsi788@users.noreply.github.com>
2022-09-23 14:14:38 +0530

    Add files via upload

---
+++ day 5 1.py
@@ -0,0 +1,14 @@
+def lengthOfLastWord(a):
+	l = 0
+	x = a.strip()
+
+	for i in range(len(x)):
+		if x[i] == " ":
+			l = 0
+		else:
+			l += 1
+	return l
+
+if __name__ == "__main__":
+	inp = " HEllO WORLD "
+	print("The length of last word is",lengthOfLastWord(inp))
